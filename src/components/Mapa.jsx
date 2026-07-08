93f334e8c2cb2713675fe159d79ce800e6403924
perzit0 <percy19marceliano@gmail.com>
2026-07-08 17:21:44 -0500

    fix: cambiar mapa a tema claro (light_all)

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -111,7 +111,7 @@ function Mapa() {
       style={{ height: "100%", width: "100%", borderRadius: "8px" }}
     >
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
       />
 
